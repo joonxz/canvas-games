feca2d719ef98e8e6968331c7bf16304a33d0059
Francis Vargas <fvargas213@gmail.com>
2015-08-12 13:31:24 -0400

    switched game to fixed size

--- frontend/js/resize-canvas.js
+++ frontend/js/resize-canvas.js
@@ -1,9 +1,9 @@
 // set width and height of canvas
-var resizeCanvas = function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-};
+// var resizeCanvas = function () {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// };
 
-window.addEventListener('resize', resizeCanvas, false);
+// window.addEventListener('resize', resizeCanvas, false);
 
-resizeCanvas();
+// resizeCanvas();
